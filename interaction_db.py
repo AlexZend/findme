fdffa470d532b3201274e37f0a78bbff879519ea
AlexZend <47773406+AlexZend@users.noreply.github.com>
2020-10-06 13:07:08 +0300

    Add interaction_db file
    Add a file that implements interaction with the MongoDB database

---
+++ interaction_db.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+import datetime
+import uuid
+
+
+MongoURI = 'mongodb://<dbuser>:<dbpassword>@<URI>/<database>' // To connect using a driver via the standard MongoDB URI
+
+client = MongoClient(MongoURI, retryWrites=False)
+findmedstu = client.findmedstu
+
+requests = findmedstu.requests
+
+def addRequestToDB(id_photo, ids_result_persons):
+    document = {
+        'id_photo': id_photo,
+        'user_confirmation': False,
+        'admin_confirmation': False,
+        'ids_result_persons': ids_result_persons,
+        'date_time': datetime.datetime.utcnow()
+    }
+    requests.insert_one(document)
+
+def updateRequest(request_id, face_card_id):
+    requests.update_one(
+        {'id_photo': uuid.UUID(request_id)},
+        {'$set':
+            {
+                "user_confirmation": True,
+                "user_confirmed_card_id": face_card_id
+            }
+        }
+    )
+    print('status confirm: ok')
